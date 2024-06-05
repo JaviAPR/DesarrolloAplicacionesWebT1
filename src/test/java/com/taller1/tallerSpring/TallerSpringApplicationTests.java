06977714c5bc6691c925298fe3c4621ee07b886c
JavierP <132411728+JaviAPR@users.noreply.github.com>
2024-06-04 20:53:10 -0500

    Archivos restantes agregados

---
+++ src/test/java/com/taller1/tallerSpring/TallerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taller1.tallerSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
