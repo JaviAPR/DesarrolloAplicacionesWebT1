07f84775dfda4da5b5b2c0f7f8307bd87637c83a
JavierP <132411728+JaviAPR@users.noreply.github.com>
2024-06-16 03:11:03 -0500

    Adicion del archivo JSON de Insomnia para dejar evidencia de las peticiones realizadas

--- baseDatos.sql
+++ baseDatos.sql
@@ -1,2 +1,9 @@
 create database if not exists taller1unir;
 use taller1unir;
+describe producto;
+describe ventas;
+select * from producto;
+select * from ventas;
+
+drop table producto;
+drop table ventas;
